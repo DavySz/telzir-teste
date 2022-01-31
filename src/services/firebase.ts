f1fd990fb0fcbab3fcce0d44323fb6b7caaa7933
Davy de Souza Assunção <davydesouzabar@gmail.com>
2022-01-30 21:12:42 -0400

    Firebase Hosting

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8tou25X9zuBMZJOSp9qbdkqi_bKHQeEA",
-    authDomain: "telzir-loldesign.firebaseapp.com",
-    projectId: "telzir-loldesign",
-    storageBucket: "telzir-loldesign.appspot.com",
-    messagingSenderId: "461365965600",
-    appId: "1:461365965600:web:86a187505a163a6735dd45"
+    //your config
 };
 
 const app = initializeApp(firebaseConfig);
